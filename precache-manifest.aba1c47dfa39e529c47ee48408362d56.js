062712e4ee16b6cfbc9754ebbb23a3033bfbe813
Drod86 <Daniel.r.rodriguez86@gmail.com>
2020-03-30 17:55:51 -0400

    Updates

---
+++ precache-manifest.aba1c47dfa39e529c47ee48408362d56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf6c748cf5176c4a2dd3bf5c2de9ae3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "ec9121468305148393b0",
+    "url": "/robofriends/static/css/main.2b09de1e.chunk.css"
+  },
+  {
+    "revision": "27d5138e769ee30802c2",
+    "url": "/robofriends/static/js/2.e636b603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e636b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9121468305148393b0",
+    "url": "/robofriends/static/js/main.fea0cf8f.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
